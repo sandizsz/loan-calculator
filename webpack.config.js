172c575075d4d8fcae45f6faa62a5310814eee62
sandizsz <sandissirmais8@gmail.com>
2025-02-08 18:13:29 +0200

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'src'),
     filename: 'index.js'
   },
   externals: {
