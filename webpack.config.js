645e3acc929e204be5af704ea49faade35ce50ac
sandizsz <sandissirmais8@gmail.com>
2025-02-08 20:22:37 +0200

    FIx tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'index.js'
   },
   externals: {
