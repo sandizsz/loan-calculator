62703dde4fb250ce177c78fad093a6ce1640e843
sandizsz <sandissirmais8@gmail.com>
2025-02-09 22:36:34 +0200

    Change text

--- src/components/FullCalculator.js
+++ src/components/FullCalculator.js
@@ -433,7 +433,7 @@ function FullCalculator() {
 
     return (
         <div className="max-w-2xl mx-auto p-6 rounded-xl shadow-lg">
-            <h2 className="text-2xl font-bold text-gray-900 mb-6">Pieteikums kredītu salīdzināšanai</h2>
+            <h2 className="text-2xl font-bold text-gray-900 mb-6">Aizpildi pieteikumu</h2>
             <p className="text-gray-600 mb-8">Aizpildīšanas laiks var aizņemt līdz 5 minūtēm.</p>
             
             <ProgressBar currentStep={currentStep} totalSteps={TOTAL_STEPS} />
