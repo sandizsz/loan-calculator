a020a852cadebf7461ed1e606ed636822d667a16
sandizsz <sandissirmais8@gmail.com>
2025-04-12 18:13:55 +0300

    check speed

--- src/components/LoanCalculator.js
+++ src/components/LoanCalculator.js
@@ -420,7 +420,7 @@ const LoanCalculator = () => {
     };
 
     if (isLoading) {
-        return <div className="p-4 text-center">Loading calculator...</div>;
+        return <div className="p-4 text-center"></div>;
     }
 
     return (
